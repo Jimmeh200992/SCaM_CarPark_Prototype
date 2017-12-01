d17f7f02e2c128607285df3bd6f508aaa118a8cf
b6018048 <b6018048@9309-16J522054>
2017-12-01 11:53:23 +0000

    Added classes, built sensor class

---
+++ SCaM_CarPark_Prototype/ChipCoinComponents/PayMachine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCaM_CarPark_Prototype
+{
+    class PayMachine
+    {
+    }
+}
